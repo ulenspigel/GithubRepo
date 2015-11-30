b57789937d263ae1c8ae8a0514a449d9377ca30b
ulenspigel <dmytro.kovalov@db.com>
2015-11-30 14:56:26 +0200

    Tests coverage extended

--- JavaCourses/Lesson1/NumConverter/src/courses/lesson1/converter/NumConverter.java
+++ JavaCourses/Lesson1/NumConverter/src/courses/lesson1/converter/NumConverter.java
@@ -2,8 +2,8 @@ package courses.lesson1.converter;
 
 public class NumConverter {
 
-    private static final int MAX_FRACTIONAL_DIGITS = 6;
-    private static final char DECIMAL_SEPARATOR = '.';
+    public static final int MAX_FRACTIONAL_DIGITS = 6;
+    public static final char DECIMAL_SEPARATOR = '.';
 
     public static String intToString(int number) {
         String result = new String();
@@ -20,8 +20,7 @@ public class NumConverter {
         return result;
     }
 
-    public static int stringToInt(String number) {
-        // TODO: overflow
+    public static int stringToInt(String number) throws IntOverflowException {
         int result = 0;
         boolean isNegative = false;
         if (number.charAt(0) == '-') {
@@ -29,8 +28,12 @@ public class NumConverter {
             number = number.substring(1);
         }
 
-        for (int i = 0; i < number.length(); i++)
-            result += Character.getNumericValue(number.charAt(i)) * Math.pow(10, number.length() - 1 - i);
+        for (int i = 0; i < number.length(); i++) {
+            int digitInCurrentPosition = Character.getNumericValue(number.charAt(i));
+            result += digitInCurrentPosition * Math.pow(10, number.length() - 1 - i);
+            if (result == Integer.MAX_VALUE && (i < (number.length() - 1) || digitInCurrentPosition > Integer.MAX_VALUE % 10))
+                throw new IntOverflowException("String representation contains number greater than max allowed");
+        }
 
         if (isNegative)
             result *= -1;
@@ -39,23 +42,29 @@ public class NumConverter {
     }
 
     public static String doubleToString(double number) {
-        int integral = (int) number;
-        int fractional = (int) Math.round(Math.abs(number - integral) * Math.pow(10, MAX_FRACTIONAL_DIGITS));
+        int integral = (int) Math.abs((int) number);
+        int fractional = (int) Math.round((Math.abs(number) - integral) * Math.pow(10, MAX_FRACTIONAL_DIGITS));
         String integralString = intToString(integral);
         String fractionalString = intToString(fractional).replaceAll("0+$", "");
-        String result = integralString + (fractionalString.equals("") ? "" : DECIMAL_SEPARATOR) + fractionalString;
+        String result = (number < 0 ? "-" : "") + integralString +
+                (fractionalString.equals("") ? "" : DECIMAL_SEPARATOR) + fractionalString;
 
         return result;
     }
 
-    public static double stringToDouble(String number) {
+    public static double stringToDouble(String number) throws IntOverflowException {
         double result = 0;
+
         int separatorPosition = number.indexOf(DECIMAL_SEPARATOR);
         if (separatorPosition > 0) {
             String integralString = number.substring(0, number.indexOf(DECIMAL_SEPARATOR));
             String fractionalString = number.substring(number.indexOf(DECIMAL_SEPARATOR) + 1);
+
+            if (fractionalString.length() > MAX_FRACTIONAL_DIGITS)
+                fractionalString = fractionalString.substring(0, MAX_FRACTIONAL_DIGITS);
+
             int integral = stringToInt(integralString);
-            double fractional = Math.signum(integral) *
+            double fractional = (number.charAt(0) == '-' ? -1 : 1) *
                     stringToInt(fractionalString) / Math.pow(10, fractionalString.length());
             result = integral + fractional;
         } else {
@@ -65,7 +74,7 @@ public class NumConverter {
         return result;
     }
 
-    public static void main(String... args) {
+    public static void main(String... args) throws Exception {
 
         /*String testNumber = new String("610");
         System.out.println("Initial string      : " + testNumber +
@@ -75,9 +84,18 @@ public class NumConverter {
         System.out.println("Initial double number: " + testNumber +
                            "\nResult of converting : " + doubleToString(testNumber));*/
 
-        String testNumber = new String("0");
+        /*String testNumber = new String("0");
         System.out.println("Initial string      : " + testNumber +
-                           "\nResult of converting: " + stringToDouble(testNumber));
+                           "\nResult of converting: " + stringToDouble(testNumber));*/
+
+        System.out.println(stringToInt("2147483647"));
+        //System.out.println("\"" + Integer.MAX_VALUE + "\"");
     }
 
+}
+
+class IntOverflowException extends Exception {
+    public IntOverflowException(String message) {
+        super(message);
+    }
 }
--- JavaCourses/Lesson1/NumConverter/test/courses/lesson1/converter/NumConverterTest.java
+++ JavaCourses/Lesson1/NumConverter/test/courses/lesson1/converter/NumConverterTest.java
@@ -6,7 +6,6 @@ import static org.junit.Assert.*;
 public class NumConverterTest {
     @Test
     public void testIntToString() {
-        // TODO: test overflow
         assertEquals("0", NumConverter.intToString(0));
         assertEquals("3", NumConverter.intToString(3));
         assertEquals("-5", NumConverter.intToString(-5));
@@ -14,13 +13,43 @@ public class NumConverterTest {
         assertEquals("-547332", NumConverter.intToString(-547332));
     }
 
-    @Test
-    public void testStringToInt() {
-        // TODO: test overflow
+    @Test (expected = IntOverflowException.class)
+    public void testStringToInt() throws IntOverflowException {
         assertEquals(0, NumConverter.stringToInt("0"));
         assertEquals(7, NumConverter.stringToInt("7"));
         assertEquals(-8, NumConverter.stringToInt("-8"));
         assertEquals(712353, NumConverter.stringToInt("712353"));
         assertEquals(-235713, NumConverter.stringToInt("-235713"));
+        assertEquals(Integer.MAX_VALUE, NumConverter.stringToInt(String.valueOf(Integer.MAX_VALUE)));
+        assertNotEquals(Integer.MAX_VALUE, NumConverter.stringToInt(String.valueOf(Integer.MAX_VALUE + 1)));
+        assertNotEquals(Integer.MAX_VALUE, NumConverter.stringToInt(String.valueOf(Integer.MAX_VALUE) + "123"));
+    }
+
+    @Test
+    public void testDoubleToString() {
+        assertEquals("0", NumConverter.doubleToString(0));
+        assertEquals("0", NumConverter.doubleToString(0.0));
+        assertEquals("123", NumConverter.doubleToString(123));
+        assertEquals("-321", NumConverter.doubleToString(-321));
+        assertEquals("0" + NumConverter.DECIMAL_SEPARATOR + "321", NumConverter.doubleToString(0.321));
+        assertEquals("-0" + NumConverter.DECIMAL_SEPARATOR + "456", NumConverter.doubleToString(-0.456));
+        assertEquals("46352" + NumConverter.DECIMAL_SEPARATOR + "1462", NumConverter.doubleToString(46352.1462));
+        assertEquals("-743" + NumConverter.DECIMAL_SEPARATOR + "6862", NumConverter.doubleToString(-743.6862));
+        assertEquals("2354" + NumConverter.DECIMAL_SEPARATOR + "121642", NumConverter.doubleToString(2354.121641843146));
+    }
+
+    @Test (expected = IntOverflowException.class)
+    public void testStringToDouble() throws IntOverflowException {
+        assertEquals(0, NumConverter.stringToDouble("0"), Math.pow(10, -NumConverter.MAX_FRACTIONAL_DIGITS - 1));
+        assertEquals(0, NumConverter.stringToDouble("0.0"), Math.pow(10, -NumConverter.MAX_FRACTIONAL_DIGITS - 1));
+        assertEquals(5674, NumConverter.stringToDouble("5674"), Math.pow(10, -NumConverter.MAX_FRACTIONAL_DIGITS - 1));
+        assertEquals(-9813, NumConverter.stringToDouble("-9813"), Math.pow(10, -NumConverter.MAX_FRACTIONAL_DIGITS - 1));
+        assertEquals(0.6124, NumConverter.stringToDouble("0" + NumConverter.DECIMAL_SEPARATOR + "6124"), Math.pow(10, -NumConverter.MAX_FRACTIONAL_DIGITS - 1));
+        assertEquals(-0.23547, NumConverter.stringToDouble("-0" + NumConverter.DECIMAL_SEPARATOR + "23547"), Math.pow(10, -NumConverter.MAX_FRACTIONAL_DIGITS - 1));
+        assertEquals(-2341234.512311, NumConverter.stringToDouble("-2341234" + NumConverter.DECIMAL_SEPARATOR + "5123112521"), Math.pow(10, -NumConverter.MAX_FRACTIONAL_DIGITS - 1));
+        assertEquals(2147483647.12345, NumConverter.stringToDouble("2147483647" + NumConverter.DECIMAL_SEPARATOR + "12345"), Math.pow(10, -NumConverter.MAX_FRACTIONAL_DIGITS - 1));
+        assertNotEquals(2147483648.12345, NumConverter.stringToDouble("2147483648" + NumConverter.DECIMAL_SEPARATOR + "12345"));
+        assertNotEquals(21474836471.12345, NumConverter.stringToDouble("21474836471" + NumConverter.DECIMAL_SEPARATOR + "12345"));
+        assertEquals(0.214748, NumConverter.stringToDouble("0" + NumConverter.DECIMAL_SEPARATOR + "2147483648"), Math.pow(10, -NumConverter.MAX_FRACTIONAL_DIGITS - 1));
     }
 }
