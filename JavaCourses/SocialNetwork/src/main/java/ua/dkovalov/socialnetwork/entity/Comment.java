1e15d99b56846ceea1b27498c33c84043d83c187
ulenspigel <dmytro.kovalov@db.com>
2016-04-29 19:29:45 +0300

    SocialNetwork: hibernate mappings for Post and Comment entities

--- JavaCourses/SocialNetwork/src/main/java/ua/dkovalov/socialnetwork/entity/Comment.java
+++ JavaCourses/SocialNetwork/src/main/java/ua/dkovalov/socialnetwork/entity/Comment.java
@@ -1,4 +1,79 @@
 package ua.dkovalov.socialnetwork.entity;
 
+import javax.persistence.*;
+import java.sql.Date;
+
+// TODO: common stereotype-table for comments and posts with common fields that allows provide self reference
+// and make possible to link comments to posts and other comments
+
+@Entity
+@Table(name = "t_comment")
 public class Comment {
+    private Integer commentId;
+    private Date commentDate;
+    private User author;
+    private Post parent;
+    private String commentText;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "comment_id")
+    public Integer getCommentId() {
+        return commentId;
+    }
+
+    public void setCommentId(Integer commentId) {
+        this.commentId = commentId;
+    }
+
+    @Column(name = "comment_date")
+    public Date getCommentDate() {
+        return commentDate;
+    }
+
+    public void setCommentDate(Date commentDate) {
+        this.commentDate = commentDate;
+    }
+
+    @ManyToOne
+    @JoinColumn(name = "author_id")
+    public User getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(User author) {
+        this.author = author;
+    }
+
+    @ManyToOne
+    @JoinColumn(name = "parent_post_id")
+    public Post getParent() {
+        return parent;
+    }
+
+    public void setParent(Post parent) {
+        this.parent = parent;
+    }
+
+    @Column(name = "comment_text")
+    public String getCommentText() {
+        return commentText;
+    }
+
+    public void setCommentText(String commentText) {
+        this.commentText = commentText;
+    }
+
+    @Override
+    public String toString() {
+        return "Comment{" +
+                "commentId=" + commentId +
+                ", commentDate=" + commentDate +
+                ", author Id=" + author.getUserId() +
+                ", author nickname=" + author.getNickname() +
+                ", post Id=" + parent.getPostId() +
+                ", post title=" + parent.getPostTitle() +
+                ", commentText='" + commentText + '\'' +
+                '}';
+    }
 }
--- JavaCourses/SocialNetwork/src/main/java/ua/dkovalov/socialnetwork/entity/Post.java
+++ JavaCourses/SocialNetwork/src/main/java/ua/dkovalov/socialnetwork/entity/Post.java
@@ -1,4 +1,74 @@
 package ua.dkovalov.socialnetwork.entity;
 
+import javax.persistence.*;
+import java.sql.Date;
+
+@Entity
+@Table(name = "t_post")
 public class Post {
+    private Integer postId;
+    private User author;
+    private Date submissionDate;
+    private String postTitle;
+    private String postContent;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "post_id")
+    public Integer getPostId() {
+        return postId;
+    }
+
+    public void setPostId(Integer postId) {
+        this.postId = postId;
+    }
+
+    @ManyToOne
+    @JoinColumn(name = "author_id")
+    public User getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(User author) {
+        this.author = author;
+    }
+
+    @Column(name = "submission_date")
+    public Date getSubmissionDate() {
+        return submissionDate;
+    }
+
+    public void setSubmissionDate(Date submissionDate) {
+        this.submissionDate = submissionDate;
+    }
+
+    @Column(name = "post_title")
+    public String getPostTitle() {
+        return postTitle;
+    }
+
+    public void setPostTitle(String postTitle) {
+        this.postTitle = postTitle;
+    }
+
+    @Column(name = "post_content")
+    public String getPostContent() {
+        return postContent;
+    }
+
+    public void setPostContent(String postContent) {
+        this.postContent = postContent;
+    }
+
+    @Override
+    public String toString() {
+        return "Post{" +
+                "postId=" + postId +
+                ", author ID=" + author.getUserId() +
+                ", author nickname=" + author.getNickname() +
+                ", submissionDate=" + submissionDate +
+                ", postTitle='" + postTitle + '\'' +
+                ", postContent='" + postContent + '\'' +
+                '}';
+    }
 }
